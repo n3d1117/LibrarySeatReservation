998f2e2c63045080ceca49c16c239b219aa6ecbc
ned <ned@me.com>
2021-07-09 12:00:27 +0200

    updated startup bean

--- backend/src/main/java/startup/StartupBean.java
+++ backend/src/main/java/startup/StartupBean.java
@@ -1,6 +1,8 @@
 package startup;
 
+import dao.LibraryDao;
 import dao.UserDao;
+import model.Library;
 import model.ModelFactory;
 import model.Role;
 import model.User;
@@ -20,24 +22,43 @@ public class StartupBean {
     @Inject
     private UserDao userDao;
 
+    @Inject
+    private LibraryDao libraryDao;
+
     @PostConstruct
     @Transactional
     public void init() {
 
-        User adminUser = ModelFactory.initializeUser();
-        adminUser.setEmail("admin@email.com");
-        adminUser.setName("Admin");
-        adminUser.setSurname("Admin");
-        adminUser.setPassword("password");
+        User adminUser = createUser("admin@email.com", "Admin", "Admin", "password");
         adminUser.setRoles(Arrays.asList(Role.BASIC, Role.ADMIN));
         userDao.save(adminUser);
 
-        User regularUser = ModelFactory.initializeUser();
-        regularUser.setEmail("user@email.com");
-        regularUser.setName("User");
-        regularUser.setSurname("User");
-        regularUser.setPassword("password");
+        User regularUser = createUser("user@email.com", "User", "User", "pass");
         regularUser.setRoles(Collections.singletonList(Role.BASIC));
         userDao.save(regularUser);
+
+        Library bandino = createLibrary("Biblioteca Villa Bandini", "Via del Paradiso, 5", 50);
+        libraryDao.save(bandino);
+
+        Library luzi = createLibrary("Biblioteca Mario Luzi", "Via Ugo Schiff, 8 (ang. via Gabriele D'Annunzio)", 70);
+        libraryDao.save(luzi);
+    }
+
+    private User createUser(String email, String name, String surname, String password) {
+        User user = ModelFactory.initializeUser();
+        user.setEmail(email);
+        user.setName(name);
+        user.setSurname(surname);
+        user.setPassword(password);
+        user.setRoles(Arrays.asList(Role.BASIC, Role.ADMIN));
+        return user;
+    }
+
+    private Library createLibrary(String name, String address, Integer capacity) {
+        Library library = ModelFactory.initializeLibrary();
+        library.setName(name);
+        library.setAddress(address);
+        library.setCapacity(capacity);
+        return library;
     }
 }
