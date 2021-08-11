d1d07f91a81d5fc0010fa58cb7ad48f70834898b
ned <ned@me.com>
2021-08-11 19:55:37 +0200

    [frontend] updated my-reservations component

--- frontend/src/app/components/my-reservations/my-reservations.component.ts
+++ frontend/src/app/components/my-reservations/my-reservations.component.ts
@@ -4,6 +4,8 @@ import {first} from "rxjs/operators";
 import {Reservation} from "../../models/reservation.model";
 import {ReservationService} from "../../services/reservation.service";
 import {AuthenticationService} from "../../services/authentication.service";
+import {PageEvent} from "@angular/material/paginator";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-my-reservations',
@@ -16,22 +18,73 @@ export class MyReservationsComponent implements OnInit {
   reservations: Reservation[] = [];
   error = '';
 
+  displayedColumns: string[] = ['hour', 'datetime', 'libraryName', 'action'];
+  lowValue = 0;
+  highValue = 5;
+
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService,
-    private reservationService: ReservationService
+    private reservationService: ReservationService,
+    private snackBar: MatSnackBar
   ) { }
+
+  pastReservations(): Reservation[] {
+    const yesterday = new Date(Date.now() - 86400000);
+    return this.reservations.filter((item) => {
+      return this.stringToDate(item.datetime) < yesterday;
+    }).sort((a, b) => {
+      return this.stringToDate(a.datetime).getTime() - this.stringToDate(b.datetime).getTime();
+    })
+  }
+
+  futureReservations(): Reservation[] {
+    const yesterday = new Date(Date.now() - 86400000);
+    return this.reservations.filter((item) => {
+      return this.stringToDate(item.datetime) >= yesterday;
+    }).sort((a, b) => {
+      return this.stringToDate(a.datetime).getTime() - this.stringToDate(b.datetime).getTime();
+    })
+  }
 
   ngOnInit(): void {
     this.loading = true;
     this.reservationService.allByUser(this.authenticationService.currentUserValue.id)
       .pipe(first()).subscribe(reservations => {
-      this.loading = false;
       this.reservations = reservations;
+      this.loading = false;
     }, error => {
       this.loading = false;
       this.error = error;
     });
+  }
+
+  stringToDate(date: string): Date {
+    return new Date(date.replace(' ', 'T'));
+  }
+
+  public getPaginatorData(event: PageEvent): PageEvent {
+    this.lowValue = event.pageIndex * event.pageSize;
+    this.highValue = this.lowValue + event.pageSize;
+    return event;
+  }
+
+  deleteReservation(reservationId: number): void {
+    this.reservationService.delete(reservationId)
+      .pipe(first())
+      .subscribe(() => {
+        this.snackBar.open('Prenotazione cancellata!', '', {duration: 5000});
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  humanReadableDate(date: Date): string {
+    return date.toLocaleDateString('it',
+      {weekday: "long", month: "long", day: "numeric"}
+    ).split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.substring(1))
+      .join(' ');
   }
 
 }
--- frontend/src/app/models/reservation.model.ts
+++ frontend/src/app/models/reservation.model.ts
@@ -2,14 +2,16 @@ export class Reservation {
   id: number;
   userId: number;
   libraryId: number;
+  libraryName: string;
   userName: string;
   userEmail: string;
   datetime: string;
 
-  constructor(id: number, userId: number, libraryId: number, userName: string, userEmail: string, datetime: string) {
+  constructor(id: number, userId: number, libraryId: number, libraryName: string, userName: string, userEmail: string, datetime: string) {
     this.id = id;
     this.userId = userId;
     this.libraryId = libraryId;
+    this.libraryName = libraryName;
     this.userName = userName;
     this.userEmail = userEmail;
     this.datetime = datetime;
