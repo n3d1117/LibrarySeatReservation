59ae36d6cd8e4e6d0ac612466e4261fbf729fbf7
ned <ned@me.com>
2021-08-13 20:01:01 +0200

    [gateway] added gateway folder

---
+++ gateway/src/main/java/rest/Ping.java
@@ -0,0 +1,21 @@
+package rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("")
+public class Ping {
+
+    @GET
+    @Path("/ping")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response get() {
+        return Response
+                .ok()
+                .entity("It works!")
+                .build();
+    }
+}
