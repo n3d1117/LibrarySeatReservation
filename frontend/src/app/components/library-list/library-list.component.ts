0af55d6d2e806424591f1a2c6f8974b583bac6ba
Pietro Zarri <pietrozarri96@gmail.com>
2021-08-31 19:35:55 +0200

    [frontend] style reservation page

--- frontend/src/app/components/library-list/library-list.component.ts
+++ frontend/src/app/components/library-list/library-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit} from '@angular/core';
+import { ResizeEvent } from 'leaflet';
 import { Library } from 'src/app/models/library.model';
 @Component({
   selector: 'app-library-list-item',
@@ -8,9 +9,33 @@ import { Library } from 'src/app/models/library.model';
 export class LibraryListComponent implements OnInit{
 
   @Input() libraries! : Library[];
+  columns!: number;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.breakPoints();
+  }
 
+  // Cards
+  breakPoints() {
+    switch(true) {
+        case (window.innerWidth <= 380):
+          this.columns = 1;
+          break;
+        case (window.innerWidth > 380 && window.innerWidth <= 640):
+          this.columns = 2;
+          break;
+        case (window.innerWidth > 640 && window.innerWidth <= 992):
+          this.columns = 3;
+          break;
+        default:
+          this.columns = 4;
+      }
+    }
+  
+  onResize(event: any) {
+    console.log("Resize...")
+    this.breakPoints();
+  }
 }
