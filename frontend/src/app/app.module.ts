f0590d74cd7ded94666ef8be6bd16720914168f2
Pietro Zarri <pietrozarri96@gmail.com>
2021-09-01 17:17:37 +0200

    [frontend] split material components import
    add material module for material components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,52 +1,33 @@
 import { MaterialElevationDirective } from './components/library-list/material-elevation.directive';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatIconModule} from "@angular/material/icon";
-import {MatInputModule} from "@angular/material/input";
-import {MatButtonModule} from "@angular/material/button";
-import {MatCardModule} from "@angular/material/card";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { HomeComponent } from './components/home/home.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {BasicAuthInterceptor} from "./auth/basic-http-auth.interceptor";
 import {ErrorInterceptor} from "./auth/error.interceptor";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
-import {MatListModule} from "@angular/material/list";
-import {MatGridListModule} from "@angular/material/grid-list";
 import { SignupComponent } from './components/signup/signup.component';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { MyReservationsComponent } from './components/my-reservations/my-reservations.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { AddLibraryComponent } from './components/add-library/add-library.component';
 import { LibraryComponent } from './components/library/library.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
-import {MatDatepickerModule} from "@angular/material/datepicker";
-import {MAT_DATE_LOCALE, MatNativeDateModule} from '@angular/material/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { MapComponent } from './components/map/map.component';
 import { ReservationsBoxComponent } from './components/reservations-box/reservations-box.component';
-import {MatCheckboxModule} from "@angular/material/checkbox";
 import {CalendarHeader} from "./components/calendar/calendar.header.component";
-import {MatChipsModule} from "@angular/material/chips";
 import { AdminReservationsBoxComponent } from './components/admin-reservations-box/admin-reservations-box.component';
-import {MatTabsModule} from "@angular/material/tabs";
-import {MatTableModule} from "@angular/material/table";
-import {MatPaginatorModule} from "@angular/material/paginator";
-import {MatSliderModule} from "@angular/material/slider";
 import {QRCodeModule} from "angularx-qrcode";
 import { QrcodeComponent } from './components/qrcode/qrcode.component';
-import {MatBottomSheet, MatBottomSheetModule} from "@angular/material/bottom-sheet";
 import { LibraryListComponent } from './components/library-list/library-list.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -66,38 +47,19 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
     QrcodeComponent,
     LibraryListComponent,
     SearchBarComponent,
-    MaterialElevationDirective
+    MaterialElevationDirective,
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         HttpClientModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule,
-        MatButtonModule,
-        MatCardModule,
         ReactiveFormsModule,
-        MatToolbarModule,
-        MatMenuModule,
-        MatProgressSpinnerModule,
-        MatListModule,
-        MatGridListModule,
-        MatSnackBarModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
         LeafletModule,
-        MatCheckboxModule,
-        MatChipsModule,
-        MatTabsModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatSliderModule,
         FormsModule,
         QRCodeModule,
-        MatBottomSheetModule,
-        FlexLayoutModule
+        FlexLayoutModule,
+        MaterialModule
     ],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'it-IT' },
---
+++ frontend/src/app/material/material.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatTableModule} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatListModule} from "@angular/material/list";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatBottomSheetModule} from "@angular/material/bottom-sheet";
+import {MatGridListModule} from "@angular/material/grid-list";
+
+const MaterialComponents = [
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatProgressSpinnerModule,
+  MatListModule,
+  MatSnackBarModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatBottomSheetModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatTabsModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSliderModule,
+  MatGridListModule,
+]
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
