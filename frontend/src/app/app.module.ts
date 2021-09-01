07454d18469c149fa4f94e12abbb78fd4acca766
ned <ned@me.com>
2021-09-01 21:52:12 +0200

    [frontend] added missing module and other fixes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,33 +1,34 @@
-import { MaterialElevationDirective } from './components/library-list/material-elevation.directive';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialElevationDirective} from './components/library-list/material-elevation.directive';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { HomeComponent } from './components/home/home.component';
+import {HomeComponent} from './components/home/home.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {BasicAuthInterceptor} from "./auth/basic-http-auth.interceptor";
 import {ErrorInterceptor} from "./auth/error.interceptor";
-import { SignupComponent } from './components/signup/signup.component';
-import { MyReservationsComponent } from './components/my-reservations/my-reservations.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { AddLibraryComponent } from './components/add-library/add-library.component';
-import { LibraryComponent } from './components/library/library.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { MapComponent } from './components/map/map.component';
-import { ReservationsBoxComponent } from './components/reservations-box/reservations-box.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {MyReservationsComponent} from './components/my-reservations/my-reservations.component';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {AddLibraryComponent} from './components/add-library/add-library.component';
+import {LibraryComponent} from './components/library/library.component';
+import {CalendarComponent} from './components/calendar/calendar.component';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {MapComponent} from './components/map/map.component';
+import {ReservationsBoxComponent} from './components/reservations-box/reservations-box.component';
 import {CalendarHeader} from "./components/calendar/calendar.header.component";
-import { AdminReservationsBoxComponent } from './components/admin-reservations-box/admin-reservations-box.component';
+import {AdminReservationsBoxComponent} from './components/admin-reservations-box/admin-reservations-box.component';
 import {QRCodeModule} from "angularx-qrcode";
-import { QrcodeComponent } from './components/qrcode/qrcode.component';
-import { LibraryListComponent } from './components/library-list/library-list.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
-import { MaterialModule } from './material/material.module';
+import {QrcodeComponent} from './components/qrcode/qrcode.component';
+import {LibraryListComponent} from './components/library-list/library-list.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {SearchBarComponent} from './components/search-bar/search-bar.component';
+import {MAT_DATE_LOCALE} from '@angular/material/core';
+import {MaterialModule} from './material/material.module';
+import {AdminSkipQueueInterceptor} from "./auth/admin-skip-queue.interceptor";
 
 @NgModule({
   declarations: [
@@ -49,23 +50,25 @@ import { MaterialModule } from './material/material.module';
     SearchBarComponent,
     MaterialElevationDirective,
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        ReactiveFormsModule,
-        LeafletModule,
-        FormsModule,
-        QRCodeModule,
-        FlexLayoutModule,
-        MaterialModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    LeafletModule,
+    FormsModule,
+    QRCodeModule,
+    FlexLayoutModule,
+    MaterialModule
+  ],
   providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'it-IT' },
-    { provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    {provide: MAT_DATE_LOCALE, useValue: 'it-IT'},
+    {provide: HTTP_INTERCEPTORS, useClass: BasicAuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: AdminSkipQueueInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
